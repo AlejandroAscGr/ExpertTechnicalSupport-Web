5bf35183fd94899a381a87a82db8166367a7f9fb
AlejandroAscGr <2124100196@soy.utj.edu.mx>
2026-07-12 19:47:42 -0600

    test para pull
    saludos

--- backend_general/cerrar_conexion.php
+++ backend_general/cerrar_conexion.php
@@ -2,4 +2,7 @@
 // y aqui se acaba el asunto cawn
 //pues aqui cerramos el pedo cawn
 $mysqli->close();
+
+// ahi en la calzada, por avenida juarez, afuerita del oxxo te deje unos mecos tirados, pa que te hagas unos chicles.
+
 ?>
