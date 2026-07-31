7758dd436cf36356ba1bf8cdd2d266664d225805
isauSuarez <2124100174@soy.utj.edu.mx>
2026-07-31 00:27:06 -0600

    Perfil TC y DG Registro y conexion a landing

--- backend_web/validar_entrada.php
+++ backend_web/validar_entrada.php
@@ -13,16 +13,43 @@ include("../backend_general/conexion.php");
 
     $resultado = $stmt->get_result();
 
-        if ($resultado->num_rows > 0){
-            $row = $resultado->fetch_array();
-            $_SESSION['usuario'] = $row['nombresEmp']." ". $row['apellidosEmp'];
+        if ($resultado->num_rows > 0) {
 
-            $_SESSION['perfil'] = $row['rolEmp']; 
-            header("Location: ../frontend/index.html");
+    $row = $resultado->fetch_array();
 
-        }else {
+    session_regenerate_id(true);
+
+    $_SESSION['usuario'] =
+        $row['nombresEmp'] . " " . $row['apellidosEmp'];
+
+    $_SESSION['perfil'] = strtoupper(trim($row['rolEmp']));
+
+    switch ($_SESSION['perfil']) {
+
+        case "DIRECTOR":
+            header("Location: ../frontend/dg/indexdg.html");
+            exit;
+
+        case "TECNICO":
+            header("Location: ../frontend/tc/indextc.html");
+            exit;
+
+
+        // case "CL":
+        //     header("Location: ../frontend/cliente/index.php");
+        //     exit;
+        //para el caso de cliente, se puede agregar un nuevo caso aquí si es necesario.
+
+        default:
+            session_destroy();
             header("Location: ../frontend/login.html");
-        }
+            exit;
+    }
+
+} else {
+    header("Location: ../frontend/login.html");
+    exit;
+}
 
         $stmt->close();
     include("../backend_general/cerrar_conexion.php");
