e018c37060c1d6d6ea7f30a91390ed99dd590add
AlejandroBejarR <2124100185@soy.utj.edu.mx>
2026-08-12 19:14:02 -0600

    unos pedillos 3

--- backend_general/cerrar_sesion.php
+++ backend_general/cerrar_sesion.php
@@ -3,6 +3,8 @@ session_start();
 
 session_unset();
 session_destroy();
-
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    
 header("Location: /ExpertTechnicalSupport-Web/frontend/login.html");
 exit;
+?>
